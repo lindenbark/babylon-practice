8e59b609e7890a2bfb459330586d6acf21055bc2
JongChan Choi <jong@chan.moe>
2018-09-02 21:27:01 +0900

    Updates

---
+++ component---src-pages-index-tsx-5c85fdce7c9073764053.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{182:function(e,n,t){"use strict";t.r(n);var l=t(0),r=t(186);n.default=function(){return l.createElement(r.a,null,"Hello, World!")}},186:function(e,n,t){"use strict";var l=t(0),r=t(189),a=t.n(r);t(187),n.a=function(e){var n=e.children;return l.createElement(l.Fragment,null,l.createElement(a.a,null,l.createElement("title",null,"babylon-practice")),n)}},187:function(e,n,t){}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-5c85fdce7c9073764053.js.map
